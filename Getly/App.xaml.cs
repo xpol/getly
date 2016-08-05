a550f84d01144ca4b7c09a7d55b9ebaff15ece18
Xpol Wan <xpolife@gmail.com>
2016-08-05 18:17:56 +0800

    Initial code.

---
+++ Getly/App.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+using NotifyIcon = System.Windows.Forms.NotifyIcon;
+using ContextMenu = System.Windows.Forms.ContextMenu;
+using MenuItem = System.Windows.Forms.MenuItem;
+
+namespace Getly
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public sealed partial class App: IDisposable
+    {
+        private readonly NotifyIcon _tray = new NotifyIcon();
+
+        public App()
+        {
+            _tray.Icon = Getly.Properties.Resources.Tray;
+            _tray.ContextMenu = CreateTrayMenu();
+            _tray.Visible = true;
+            _tray.DoubleClick += NewTaskOrShowTasks;
+        }
+
+        private void NewTaskOrShowTasks(object sender, EventArgs e)
+        {
+            if (GetLinks(Clipboard.GetText()).Count == 0)
+            {
+                ShowTasks(sender, e);
+            }
+        }
+
+        private static List<string> GetLinks(string message) // TODO: move to util class
+        {
+            var urlRx = new Regex(@"((https?|ftp|file)\://|www.)[A-Za-z0-9\.\-]+(/[A-Za-z0-9\?\&\=;\+!'\(\)\*\-\._~%]*)*", RegexOptions.IgnoreCase);
+            var matches = urlRx.Matches(message);
+            var list = new List<string>();
+            foreach (Match match in matches)
+                list.Add(match.Value);
+            return list;
+        }
+
+        private void Cleanup(object sender, ExitEventArgs e)
+        {
+            _tray.Dispose();
+        }
+
+
+        public void Dispose()
+        {
+            _tray.Dispose();
+        }
+
+        private ContextMenu CreateTrayMenu()
+        {
+            return new ContextMenu(new[]
+            {
+                new MenuItem("Show", ShowTasks),
+                new MenuItem("Exit", Shutdown), 
+            });
+        }
+
+        private void ShowTasks(object sender, EventArgs e)
+        {
+            var taskView = new MainWindow();
+            taskView.Show();
+        }
+
+        private void Shutdown(object sender, EventArgs e)
+        {
+            Current.Shutdown();
+        }
+    }
+}
---
+++ Getly/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Getly
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ObservableCollection<Task> _items;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _items = new ObservableCollection<Task>
+            {
+                new Task() {Name = "John Doe", Speed = 42, Progress = 0.5f},
+                new Task() {Name = "Jane Doe", Speed = 39, Progress = 0.0f},
+                new Task() {Name = "Sammy Doe", Speed = 7, Progress = 1.0f}
+            };
+            Tasks.ItemsSource = _items;
+        }
+
+        private void AddDummyTask(object sender, MouseButtonEventArgs e)
+        {
+            _items.Add(new Task() { Name = "Sammy Doe", Speed = 7, Progress = 1.0f });
+        }
+    }
+
+    public class Task
+    {
+        public string Name { get; set; }
+
+        public float Speed { get; set; }
+
+        public float Progress { get; set; }
+    }
+}
